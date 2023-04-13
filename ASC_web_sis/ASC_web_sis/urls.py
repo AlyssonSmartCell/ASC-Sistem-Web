581fad172a71d239f691c7c9cf9dfc1ad9e65328
AlyssonSmartCell <alyssonsmartcell@gmail.com>
2023-04-13 09:44:11 -0300

    teste 1

--- ASC_web_sis/ASC_web_sis/urls.py
+++ ASC_web_sis/ASC_web_sis/urls.py
@@ -25,7 +25,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('conta/', views_users.novo_usuario, name='novo_usuario'),
     path('',auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
-    path('logout/',auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
+    path(' /',auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
 
 
     
